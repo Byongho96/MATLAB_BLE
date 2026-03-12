45433ea3f61cf0f69e065d1c3df9f8b1f4c9342a
Byongho96 <unlike96@gmail.com>
2026-03-12 13:36:54 +0900

    update nonlinear triangulation

--- functions/nonLinearTriangulation.m
+++ functions/nonLinearTriangulation.m
@@ -1,50 +1,59 @@
-function posEst = nonLinearTriangulation(locators, measuredAngles, initialPos)
-    % NONLINEARTRIANGULATION 비선형 최소제곱법을 이용한 3D 위치 추정 (가중치 미적용)
-    %
-    % [입력 파라미터]
-    % - locators: 활성 로케이터 위치 (3 x N 행렬)
-    % - measuredAngles: 측정된 [Azimuth, Elevation] (N x 2 행렬, 단위: 도)
-    % - initialPos: 선형 방식으로 계산된 초기 위치 [x; y; z] (3 x 1 벡터)
-    % - roomSize: 시뮬레이션 공간 크기 [L, W, H] (경계 조건 설정용)
+function posNonLin = nonLinearTriangulation(posLin, validLocs, validAngles)
+    % 입력인자:
+    %   posLin      - 선형 삼각측량으로 구한 초기 추정 위치 [x; y; z]
+    %   validLocs   - 측위에 사용된 유효 로케이터들의 위치 행렬 [3 x N]
+    %   validAngles - 측위된 글로벌 방위각/고도각 행렬 [N x 2]
 
-    % 1. 최적화 옵션 설정
-    options = optimoptions('lsqnonlin', ...
-        'Display', 'off', ...
-        'Algorithm', 'trust-region-reflective', ...
-        'FunctionTolerance', 1e-6, ...
-        'StepTolerance', 1e-6);
-
-    % 3. 비선형 최적화 수행
-    % 가중치 없이 각도 잔차만 전달합니다.
-    try
-        posEst = lsqnonlin(@(p) angleResiduals(p, locators, measuredAngles), ...
-                           initialPos, [], [], options);
-    catch
-        % 최적화 수렴 실패 시 초기값(선형 추정값) 유지
-        posEst = initialPos;
-    end
-end
-
-function res = angleResiduals(p, locators, measuredAngles)
-    % 잔차 계산을 위한 내부 함수
-    numLocs = size(locators, 2);
-    res = zeros(numLocs * 2, 1);
+    % 1. 최적화 옵션 설정 (콘솔 출력 끄기, LM 알고리즘 지정)
+    options = optimoptions('lsqnonlin', 'Display', 'off', 'Algorithm', 'levenberg-marquardt');
     
-    for i = 1:numLocs
-        % 현재 추정 위치 p에서 로케이터 i를 바라보는 상대 벡터
-        relVec = p - locators(:, i);
+    % 2. 잔차(Residual) 계산을 위한 익명 함수 정의
+    % 내부 함수인 computeAngleResiduals를 호출하여 오차 벡터를 반환
+    residualFunc = @(p) computeAngleResiduals(p, validLocs, validAngles);
+    
+    % 3. 비선형 최소자승법 실행 (posLin을 훌륭한 '초기값'으로 사용)
+    try
+        [posEst, ~, ~, exitflag] = lsqnonlin(residualFunc, posLin, [], [], options);
         
-        % 이론적 각도 계산 (Cartesian -> Spherical)
-        [calcAzRad, calcElRad, ~] = cart2sph(relVec(1), relVec(2), relVec(3));
-        calcAz = rad2deg(calcAzRad);
-        calcEl = rad2deg(calcElRad);
+        % exitflag > 0 이면 최적화가 성공적으로 수렴했다는 뜻
+        if exitflag > 0
+            posNonLin = posEst;
+        else
+            % 수렴 실패(반복 횟수 초과 등) 시 튀는 것을 방지하기 위해 선형 결과 유지
+            posNonLin = posLin; 
+        end
+    catch
+        % 행렬 연산 에러 등 예기치 못한 오류 발생 시 안전하게 선형 결과 유지
+        posNonLin = posLin; 
+    end
+    
+    % ---------------------------------------------------------------------
+    % 내부 중첩 함수 (Nested Function): 각도 잔차 계산
+    % ---------------------------------------------------------------------
+    function res = computeAngleResiduals(p, locs, anglesGt)
+        numLocs = size(locs, 2);
+        res = zeros(numLocs * 2, 1); 
         
-        % 실제 측정 각도와 계산된 각도의 차이 (잔차)
-        % Azimuth: 360도 환상성을 고려한 각도 차이 계산 (angdiff 사용)
-        azDiff = rad2deg(angdiff(deg2rad(measuredAngles(i, 1)), deg2rad(calcAz)));
-        elDiff = measuredAngles(i, 2) - calcEl;
-        
-        res(2*i-1) = azDiff;
-        res(2*i)   = elDiff;
+        for i = 1:numLocs
+            % 로케이터에서 현재 탐색 위치(p)를 향하는 3D 벡터
+            dirVec = p - locs(:, i);
+            
+            % 기하학적 각도 계산
+            [az_rad, el_rad, ~] = cart2sph(dirVec(1), dirVec(2), dirVec(3));
+            az_exp = rad2deg(az_rad);
+            el_exp = rad2deg(el_rad);
+            
+            % 측정된 각도
+            az_meas = anglesGt(i, 1);
+            el_meas = anglesGt(i, 2);
+            
+            % 각도 래핑 (360도 경계선 오차 방지: 무조건 -180 ~ 180도로 맞춤)
+            az_diff = mod((az_meas - az_exp) + 180, 360) - 180;
+            el_diff = mod((el_meas - el_exp) + 180, 360) - 180;
+            
+            % 잔차 벡터 적재
+            res(2*i - 1) = az_diff;
+            res(2*i)     = el_diff;
+        end
     end
 end
